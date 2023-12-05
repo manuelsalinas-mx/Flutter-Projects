525da031326b8f4a686c9aa672fb0787e76cc8db
Manu Salinas <manuelsalinas.mx@gmail.com>
2023-12-05 16:36:40 -0600

    Named routes setup

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -59,7 +61,8 @@ class _CustomListTile extends StatelessWidget {
         //Navigator.pushNamed(context, item.link);
 
         // Navegar a otra pantalla - GoRouter (el favorito)
-        context.push(item.link);
+        //context.push(item.link);
+        context.pushNamed(CardsScreen.name);
       },
     );
   }
