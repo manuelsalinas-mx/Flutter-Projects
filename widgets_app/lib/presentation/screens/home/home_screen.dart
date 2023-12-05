3b8011fb806116a3bf95c4535898754e4822028f
Manu Salinas <manuelsalinas.mx@gmail.com>
2023-12-05 16:27:17 -0600

    GoRouter navigation setup

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -56,9 +56,10 @@ class _CustomListTile extends StatelessWidget {
         );
         */
         // Navegar a otra pantalla - Rutas (check main.dart)
-        Navigator.pushNamed(context, item.link);
+        //Navigator.pushNamed(context, item.link);
 
         // Navegar a otra pantalla - GoRouter (el favorito)
+        context.push(item.link);
       },
     );
   }
---
+++ widgets_app/lib/presentation/screens/screens.dart
@@ -0,0 +1,4 @@
+// Exportar todas las pantallas
+export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+export 'package:widgets_app/presentation/screens/home/home_screen.dart';
