6436ef8258d2fbc8e31bb7a0478e29de5032e12c
Manu Salinas <manuelsalinas.mx@gmail.com>
2023-12-11 12:28:01 -0600

    Inifnite scroll + pull to refresh + scroll to bottom

--- widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,17 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   static const name = 'infinite_scroll_screen';
   const InfiniteScrollScreen({super.key});
-  
 
-   @override
+  @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+  final scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        // Load next page
+        loadNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  Future<void> loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveMore();
+    isLoading = false;
+
+    // Revisar si el widget esta montado
+    if (!isMounted) return;
+    setState(() {});
+
+    // Mover scroll automaticamente
+    scrollToBottom();
+  }
+
+  void addFiveMore() {
+    final last = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => last + e));
+  }
+
+  void scrollToBottom() {
+    if ((scrollController.position.pixels + 150) <=
+        scrollController.position.maxScrollExtent) return;
+
+    scrollController.animateTo(scrollController.position.pixels + 120,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.fastOutSlowIn);
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+    if (!isMounted) return;
+
+    isLoading = false;
+    final last = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(last + 1);
+    addFiveMore();
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Infinite Scroll Screen'),
+        title: const Text('Infinite Scroll & Pull'),
       ),
-      body: const Placeholder(),
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        edgeOffset: 5,
+        strokeWidth: 2,
+        child: ListView.builder(
+            controller: scrollController,
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 300,
+                  placeholder:
+                      const AssetImage('assets/images/jar-loading.gif'),
+                  image: NetworkImage(
+                      'https://picsum.photos/id/${imagesIds[index]}/500/300'));
+            }),
+      ),
+      floatingActionButton: isLoading
+          ? const FloatingActionButton(
+              onPressed: null, child: CircularProgressIndicator())
+          : null,
     );
   }
 }
