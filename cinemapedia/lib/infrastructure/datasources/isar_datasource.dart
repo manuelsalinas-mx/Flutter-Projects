46abc539519a84d7c2760ae96a470d3acc3dc54e
Manu Salinas <manuelsalinas.mx@gmail.com>
2023-12-20 13:39:28 -0600

    Isar Datasource implementation

--- cinemapedia/lib/infrastructure/datasources/isar_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/isar_datasource.dart
@@ -1,7 +1,7 @@
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:isar/isar.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:path_provider/path_provider.dart' show getApplicationDocumentsDirectory;
 
 class IsarDatasource extends LocalStorageDatasource {
   late Future<Isar> db;
@@ -42,16 +42,15 @@ class IsarDatasource extends LocalStorageDatasource {
   Future<void> toggleFavorite(Movie movie) async {
     final isar = await db;
 
-    final favoriteMovie = isar.movies.filter().idEqualTo(movie.id).findFirst();
+    final favoriteMovie = await isar.movies.filter().idEqualTo(movie.id).findFirst();
 
-    // Existe?
+    // Si existe 'Borrar'
     if (favoriteMovie != null) {
-      // Borrar
       isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
       return;
     }
 
     // No existe entonces 'Insertar'
-    isar.writeTxnSync(() => isar.movies.put(movie));
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
   }
 }
