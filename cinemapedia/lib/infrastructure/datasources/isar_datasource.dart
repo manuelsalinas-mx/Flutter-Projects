93f32f4bc0572b86a32f764b5b5336cc4806c701
Manu Salinas <manuelsalinas.mx@gmail.com>
2023-12-20 13:18:42 -0600

    Isar Datasource implementation

---
+++ cinemapedia/lib/infrastructure/datasources/isar_datasource.dart
@@ -0,0 +1,57 @@
+import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class IsarDatasource extends LocalStorageDatasource {
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    // Directory path
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema],
+          inspector: true, directory: dir.path);
+    }
+
+    return Future.value(Isar.getInstance());
+  }
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+  @override
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await db;
+    final Movie? isFavoriteMovie =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+
+    return isFavoriteMovie != null;
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await db;
+    return isar.movies.where().offset(offset).limit(limit).findAll();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await db;
+
+    final favoriteMovie = isar.movies.filter().idEqualTo(movie.id).findFirst();
+
+    // Existe?
+    if (favoriteMovie != null) {
+      // Borrar
+      isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
+      return;
+    }
+
+    // No existe entonces 'Insertar'
+    isar.writeTxnSync(() => isar.movies.put(movie));
+  }
+}
