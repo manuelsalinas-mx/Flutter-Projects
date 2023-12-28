9544315bce02d9b59d054771759195ddc7976ac9
Manu Salinas <manuelsalinas.mx@gmail.com>
2023-12-28 12:02:00 -0600

    Products navigation to Product Detail

--- teslo_app/lib/features/products/presentation/screens/products_screen.dart
+++ teslo_app/lib/features/products/presentation/screens/products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/features/products/presentation/providers/providers.dart';
 import 'package:teslo_shop/features/products/presentation/widgets/widgets.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
@@ -75,7 +76,10 @@ class _ProductsViewState extends ConsumerState<_ProductsView> {
           itemCount: productsState.products.length,
           itemBuilder: (context, index) {
             final product = productsState.products[index];
-            return ProductCard(product: product);
+            return GestureDetector(
+              child: ProductCard(product: product),
+              onTap: () => context.push('/product/${product.id}')
+              );
           }),
     );
   }
